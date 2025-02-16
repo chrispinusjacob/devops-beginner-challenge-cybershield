aeab25a05b3d6681a134279128b3973df0a04cc3
CJ <chrispinusjacob@gmail.com>
2025-02-17 02:38:23 +0300

    removed env

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-require('dotenv').config();     
-
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, DevOps! Updated'); // Changed text for testing
+app.get("/", (req, res) => {
+    res.send("Hello, DevOps!");
 });
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
+
