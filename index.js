8826fb25f367a5bacfcda759e99520a9ffece05d
CJ <chrispinusjacob@gmail.com>
2025-02-17 01:23:11 +0300

    Test CI/CD pipeline: updated greeting text

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, DevOps!');
+  res.send('Hello, DevOps! Updated'); // Changed text for testing
 });
 
 app.listen(port, () => {
