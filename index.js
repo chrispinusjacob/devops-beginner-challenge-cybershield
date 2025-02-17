4a0534422471ee87abc81842153a0b608b62febb
CJ <chrispinusjacob@gmail.com>
2025-02-17 04:25:36 +0300

    added tst to k dev ops

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, DevOps!  tst");
+    res.send("Hello, DevOps!  tst k");
 });
 
 const PORT = process.env.PORT || 5000;
