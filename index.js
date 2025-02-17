341045f3733ff38a3df5bf04ef7c7c0c0d5dde86
CJ <chrispinusjacob@gmail.com>
2025-02-17 04:29:12 +0300

    added  devops

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, DevOps!  tst k");
+    res.send("Hello, DevOps!");
 });
 
 const PORT = process.env.PORT || 5000;
